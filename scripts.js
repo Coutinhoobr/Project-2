41b7d566136557ba5533161726cec44a40b52d1e
Coutinhoobr <coutinhoobr97@gmail.com>
2023-01-04 23:48:41 -0300

    Finalizando project

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 const button = document.getElementById("buttonConverter");
+const select = document.getElementById("selectValue");
 
-const dolar = 5.2;
+const dolar = 5.43;
+const euro = 5.77;
+const bitCoin = 91415.24;
 
-const convertValues = () => {
+convertValues = () => {
   const inputReais = document.getElementById("inputValue").value;
   const reaisValue = document.getElementById("reaisValue");
   const currencyValue = document.getElementById("currencyValue");
@@ -12,10 +15,50 @@ const convertValues = () => {
     currency: "BRL",
   }).format(inputReais);
 
-  currencyValue.innerHTML = new Intl.NumberFormat("es-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(inputReais / dolar);
+  if (select.value === "US$ Dolar Americano") {
+    currencyValue.innerHTML = new Intl.NumberFormat("es-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputReais / dolar);
+  }
+
+  if (select.value === "€ Euro") {
+    currencyValue.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputReais / euro);
+  }
+
+  if (select.value === "₿ BitCoin") {
+    currencyValue.innerHTML = new Intl.NumberFormat("es-US", {
+        maximumFractionDigits:6,
+        style:"currency",
+        currency:"BTC",
+    
+    
+    }).format(inputReais / bitCoin);
+  }
+};
+
+selectValue = () => {
+  const currencyName = document.getElementById("currencyName");
+  const currencyImg = document.getElementById("currencyImg");
+
+  if (select.value === "€ Euro") {
+    currencyName.innerHTML = "€ Euro";
+    currencyImg.src = "./assets/euro.png";
+  }
+
+  if (select.value === "₿ BitCoin") {
+    currencyName.innerHTML = "₿ BitCoin";
+    currencyImg.src = "./assets/bitcoin.png";
+  }
+  if (select.value === "US$ Dolar Americano") {
+    currencyName.innerHTML = "US$ Dolar Americano";
+    currencyImg.src = "./assets/estados-unidos.png";
+  }
+  convertValues();
 };
 
 button.addEventListener("click", convertValues);
+select.addEventListener("change", selectValue);
